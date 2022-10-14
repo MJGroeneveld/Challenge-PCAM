d17d3eda21b473527054a22c4e9464397aa4dffd
MJGroeneveld <79461911+MJGroeneveld@users.noreply.github.com>
2022-10-15 00:54:28 +0200

    Werkende network!

--- networknet.py
+++ networknet.py
@@ -9,26 +9,49 @@ class Network(pl.LightningModule):
         super(Network, self).__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            # input = 3 x 96 x 96, output = 32 x 96 x 96 
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
+            # input = 32 x 96 x 96, output = 32 x 48 x 48 
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            #input = 32 x 48 x 48, output = 64 x 48 x 48
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
+            #input = 64 x 48 x 48, output = 64 x 24 x 24
             nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # input = 64 x 24 x 24, output = 128 x 24 x 24 
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            #input = 128 x 24 x 24, output = 128 x 12 x 12 
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #input = 128 x 12 x 12, output = 256 x 12 x 12
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            #input = 256 x 12 x 12, output = 256 x 6 x 6 
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            #input = 256 x 6 x 6, output = 512 x 6 x 6 
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            #input = 512 x 6 x 6, output = 512 x 3 x 3 
+            nn.MaxPool2d(kernel_size=2, stride=2), 
+
         )
         
         self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((3,3)), 
             nn.Flatten(),
-            #Even de vraag of we van 6400 terug naar 32 kunnen
-            nn.Linear(in_features = 6400, out_features = 32),
+            nn.Linear(in_features = 3*3*512, out_features = 128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=32),
             nn.ReLU(),
             nn.Linear(in_features = 32, out_features = 1)
         )
